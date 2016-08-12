4626706e4b74c85234970fbff451e4cac2be4dcc
caasanchezcu <caasanchezcu@unal.edu.co>
2016-08-12 18:01:49 -0500

    Working on login.

---
+++ src/main/java/unal/architecture/entity/User.java
@@ -0,0 +1,52 @@
+package unal.architecture.entity;
+
+import javax.persistence.*;
+
+@Entity
+@NamedQuery(name = "User.findByUsername", query = "Select u from User u where u.username = :username")
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String name;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
