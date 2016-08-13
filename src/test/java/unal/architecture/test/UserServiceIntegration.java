0a58473ba831a3c389dec049604de1b15b00810d
caasanchezcu <caasanchezcu@unal.edu.co>
2016-08-12 21:12:53 -0500

    Testing with TomEE.

--- src/test/java/unal/architecture/test/UserServiceIntegration.java
+++ src/test/java/unal/architecture/test/UserServiceIntegration.java
@@ -13,19 +13,23 @@ import javax.ws.rs.core.MediaType;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-public class UserServiceTest {
+public class UserServiceIntegration {
     private static final String JNDI = "java:global/SoftwareArchitectureUNAL/UserService";
     private static UserService service;
+    private static Client client;
 
     @BeforeClass
     public static void beforeClass() throws NamingException {
         Object lookup = EJBContainer.createEJBContainer().getContext().lookup(JNDI);
         assertTrue(lookup instanceof UserService);
         service = (UserService) lookup;
+
+        client = ClientBuilder.newClient();
     }
 
     @AfterClass
     public static void afterClass() throws NamingException {
+        client.close();
     }
 
     @Before
@@ -37,9 +41,10 @@ public class UserServiceTest {
     }
 
     @Test
-    public void testAdmin() {
-        service.createAdmin();
-        User user = service.findByUsername("admin");
-        assertNotNull(user);
+    public void testLogin() {
+        String result = client.target("http://localhost:8080/SoftwareArchitectureUNAL/user/")
+                .request(MediaType.APPLICATION_JSON)
+                .get(String.class);
+
     }
 }
