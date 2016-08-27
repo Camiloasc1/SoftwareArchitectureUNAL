df7c89c7db16e0983bc9c1719d537fcf4ef77e59
amrondonp <amrondonp@gmail.com>
2016-08-27 15:50:29 -0500

    Login form added

--- src/main/webapp/js/SoftwareArchitectureUNAL.js
+++ src/main/webapp/js/SoftwareArchitectureUNAL.js
@@ -1,7 +1,12 @@
 "use strict";
 var app = angular.module('SoftwareArchitectureUNAL', ['ngRoute']);
 
+
+
 app.controller('HomeController', ['$scope', '$http',  function ($scope, $http) {
+}]);
+
+app.controller('LoginController', ['$scope', '$http',  function ($scope, $http) {
 }]);
 
 app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
@@ -10,6 +15,11 @@ app.config(['$locationProvider', '$routeProvider', function ($locationProvider, 
             templateUrl: 'partials/home.html',
             controller: 'HomeController'
         })
+        .when('/login', {
+            templateUrl: 'partials/login.html',
+            controller: 'LoginController'
+        })
+
         .otherwise({redirectTo: '/'});
 
     //html5mode causes several issues when the front end is embedded with the web service.
