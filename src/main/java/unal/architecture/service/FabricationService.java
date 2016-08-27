1f5a58614f59ccc57cacffc86b783de25fd0c8ae
caasanchezcu <caasanchezcu@unal.edu.co>
2016-08-26 20:36:05 -0500

    Fabrication.

---
+++ src/main/java/unal/architecture/service/FabricationService.java
@@ -0,0 +1,20 @@
+package unal.architecture.service;
+
+import unal.architecture.entity.Fabrication;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Stateless
+public class FabricationService {
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Fabrication> findAll() {
+        TypedQuery<Fabrication> query = em.createNamedQuery("Fabrication.findAll", Fabrication.class);
+        return query.getResultList();
+    }
+}
---
+++ src/test/java/unal/architecture/test/unit/FabricationServiceTest.java
@@ -0,0 +1,41 @@
+package unal.architecture.test.unit;
+
+import org.junit.*;
+import unal.architecture.entity.Fabrication;
+import unal.architecture.service.FabricationService;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.NamingException;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class FabricationServiceTest {
+    private static final String JNDI = "java:global/SoftwareArchitectureUNAL/FabricationService";
+    private static FabricationService fabricationService;
+
+    @BeforeClass
+    public static void beforeClass() throws NamingException {
+        Object lookup = EJBContainer.createEJBContainer().getContext().lookup(JNDI);
+        assertTrue(lookup instanceof FabricationService);
+        fabricationService = (FabricationService) lookup;
+    }
+
+    @AfterClass
+    public static void afterClass() throws NamingException {
+    }
+
+    @Before
+    public void before() {
+    }
+
+    @After
+    public void after() {
+    }
+
+    @Test
+    public void findAll() {
+        List<Fabrication> products = fabricationService.findAll();
+        assertTrue(products.isEmpty());
+    }
+}
