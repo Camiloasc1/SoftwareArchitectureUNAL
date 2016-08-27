1b6810cefb2dc5a744f88beb2e27c8b27a747e9e
caasanchezcu <caasanchezcu@unal.edu.co>
2016-08-27 16:47:24 -0500

    Login with angular.

--- src/main/webapp/js/SoftwareArchitectureUNAL.js
+++ src/main/webapp/js/SoftwareArchitectureUNAL.js
@@ -1,12 +1,32 @@
 "use strict";
 var app = angular.module('SoftwareArchitectureUNAL', ['ngRoute']);
 
+app.controller('NavigationController', ['$scope', '$http', function ($scope, $http) {
+    $scope.user = {};
 
-
-app.controller('HomeController', ['$scope', '$http',  function ($scope, $http) {
+    $scope.$on('OnLogin', function (event, user) {
+        $scope.user = user;
+    });
 }]);
 
-app.controller('LoginController', ['$scope', '$http',  function ($scope, $http) {
+app.controller('HomeController', ['$scope', '$http', function ($scope, $http) {
+}]);
+
+app.controller('LoginController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    $scope.credentials = {};
+    $scope.submit = function () {
+        $http.post('auth/login', $scope.credentials)
+            .then(function (response) {
+                if (response.status === 200) {
+                    $scope.$emit('OnLogin', response.data);
+                    $location.path('/');
+                }
+                else // 204 Error
+                {
+                }
+                $scope.credentials = {};
+            });
+    }
 }]);
 
 app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
@@ -19,7 +39,6 @@ app.config(['$locationProvider', '$routeProvider', function ($locationProvider, 
             templateUrl: 'partials/login.html',
             controller: 'LoginController'
         })
-
         .otherwise({redirectTo: '/'});
 
     //html5mode causes several issues when the front end is embedded with the web service.
