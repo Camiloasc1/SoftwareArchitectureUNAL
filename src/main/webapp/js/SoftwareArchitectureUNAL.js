29c25984eb26e29645ff3c610e870a808c462d8e
caasanchezcu <caasanchezcu@unal.edu.co>
2016-08-27 13:04:58 -0500

    Base SPA.

---
+++ src/main/webapp/js/SoftwareArchitectureUNAL.js
@@ -0,0 +1,18 @@
+"use strict";
+var app = angular.module('SoftwareArchitectureUNAL', ['ngRoute']);
+
+app.controller('HomeController', ['$scope', '$http',  function ($scope, $http) {
+}]);
+
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeController'
+        })
+        .otherwise({redirectTo: '/'});
+
+    //html5mode causes several issues when the front end is embedded with the web service.
+    //$locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+}]);
