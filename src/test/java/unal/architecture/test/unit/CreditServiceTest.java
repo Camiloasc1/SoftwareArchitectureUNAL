f6664a21a6fe410fd286a1af0133f92eb7aaaea9
caasanchezcu <caasanchezcu@unal.edu.co>
2016-08-26 20:35:54 -0500

    Credit.

---
+++ src/test/java/unal/architecture/test/unit/CreditServiceTest.java
@@ -0,0 +1,41 @@
+package unal.architecture.test.unit;
+
+import org.junit.*;
+import unal.architecture.entity.Credit;
+import unal.architecture.service.CreditService;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.NamingException;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class CreditServiceTest {
+    private static final String JNDI = "java:global/SoftwareArchitectureUNAL/CreditService";
+    private static CreditService creditService;
+
+    @BeforeClass
+    public static void beforeClass() throws NamingException {
+        Object lookup = EJBContainer.createEJBContainer().getContext().lookup(JNDI);
+        assertTrue(lookup instanceof CreditService);
+        creditService = (CreditService) lookup;
+    }
+
+    @AfterClass
+    public static void afterClass() throws NamingException {
+    }
+
+    @Before
+    public void before() {
+    }
+
+    @After
+    public void after() {
+    }
+
+    @Test
+    public void findAll() {
+        List<Credit> products = creditService.findAll();
+        assertTrue(products.isEmpty());
+    }
+}
