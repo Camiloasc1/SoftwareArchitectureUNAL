f58e58a18599c3440c54ad5709bea4f4f63d7e09
caasanchezcu <caasanchezcu@unal.edu.co>
2016-10-12 15:02:14 -0500

    Product Service

---
+++ src/main/java/unal/architecture/service/ProductService.java
@@ -0,0 +1,31 @@
+package unal.architecture.service;
+
+import unal.architecture.dao.ProductDAO;
+import unal.architecture.entity.Product;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+@WebService
+public class ProductService {
+    @PersistenceContext
+    private EntityManager em;
+    @EJB
+    ProductDAO productDAO;
+
+    @WebMethod
+    public List<Product> getProducts() {
+        return productDAO.findAll();
+    }
+
+    @WebMethod
+    public Product getProduct(long id) {
+        return em.find(Product.class, id);
+    }
+}
